afa30532320b715cd95501238b35873fa802df4f
dilan saskin <dilansskn@gmail.com>
2024-08-25 17:45:19 +0300

    early state front-end

---
+++ src/info.js
@@ -0,0 +1,18 @@
+const infos = [
+    {
+      id: 1,
+      title: "İstatistik Bilgisi",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id diam maecenas ultricies mi eget. Orci nulla pellentesque dignissim enim sit. Nunc vel risus commodo viverra maecenas accumsan lacus. Et ultrices neque ornare aenean euismod elementum nisi. Volutpat diam ut venenatis tellus in metus. Magna ac placerat vestibulum lectus mauris ultrices eros. Amet cursus sit amet dictum sit amet justo donec enim. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam."
+    },
+    {
+      id: 2,
+      title: "Oyun Bilgisi",
+
+      content:
+"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id diam maecenas ultricies mi eget. Orci nulla pellentesque dignissim enim sit. Nunc vel risus commodo viverra maecenas accumsan lacus. Et ultrices neque ornare aenean euismod elementum nisi. Volutpat diam ut venenatis tellus in metus. Magna ac placerat vestibulum lectus mauris ultrices eros. Amet cursus sit amet dictum sit amet justo donec enim. Ultrices neque ornare aenean euismod elementum nisi quis eleifend quam."    },
+   
+  ];
+  
+  export default infos;
+  
